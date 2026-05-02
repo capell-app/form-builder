2a63a645aa51dbf16f3501ede3e866a63b3c0210
capell-app <appcapello@gmail.com>
2026-05-02 13:45:33 +0000

    Tweaks

---
+++ resources/lang/en/package.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'description' => 'Editor-managed forms and submissions for Capell.',
+];
---
+++ src/Enums/ResourceEnum.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Capell\Forms\Enums;
+
+enum ResourceEnum: string {}
